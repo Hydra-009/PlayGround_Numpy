e71357217a333b46644da25d05cd2ee235ef302c
Hydra <Hydra.echidna.009@gmail.com>
2023-04-12 10:26:07 +0900

    Small step

--- main.py
+++ main.py
@@ -8,5 +8,8 @@ print(x)
 x[1:, ::2] = -99 #スライス[start:stop:step]によって、リストや文字列、タプルなどのシーケンスオブジェクトの一部分を選択して取得したり別の値を代入したり
 print(x)         #2次元配列の場合、[p:q:r,x:y:z]において、p,q,rは要素であるリストへのスライス、x,y,zはさらにそのリストの中身に対するスライスとなる
 
-x.max(axis=1)
-print(x)
+y = np.arange(15, dtype=np.int64).reshape(3, 5)
+z1 = x.max(axis=1) #axis=1は行に沿った処理指定（行の中で最大値探索）
+z2 = y.max(axis=0) #axis=0は列に沿った処理指定（列の中で最大値探索）
+print(z1)
+print(z2)
